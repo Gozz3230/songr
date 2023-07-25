567592f5461dffb671e9d54e5ce512cb051c893f
Ryan Gosselin <ryan.gosselin5@gmail.com>
2023-07-25 04:20:57 +0000

    initial commit for spring init

---
+++ src/main/java/com/lab11/songr/SongrApplication.java
@@ -0,0 +1,13 @@
+package com.lab11.songr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab11/songr/SongrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab11.songr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
