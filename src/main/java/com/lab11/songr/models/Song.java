a3988324db4eadf5ba586a161256908aa368a10b
Ryan Gosselin <ryan.gosselin5@gmail.com>
2023-07-30 03:46:26 +0000

    looks like i have to nuke the files and start semi fresh since we are changing directions with our implementation

---
+++ src/main/java/com/lab11/songr/models/Song.java
@@ -0,0 +1,9 @@
+package com.lab11.songr.models;
+
+public class Song {
+}
+
+// create title, length(in seconds), trackNumber, and album.
+// relationship between Album and Songs should be appropriately set up.
+
+
